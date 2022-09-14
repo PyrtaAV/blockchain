55d69b037aea804a709cec41683c083c61822163
AlexeyPyrta <pyrta90@internet.ru>
2022-09-14 06:55:52 +0400

    add method: generateHash and method: setSecondsGeneratingBlock

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,25 @@
 import block.Block;
 import block.BlockChain;
+import util.StringUtil;
+
+import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        var sc = new Scanner(System.in);
+
+        System.out.print("Enter how many zeros the hash must start with: ");
+        int countZero = Integer.parseInt(sc.nextLine());
+        System.out.println();
+
         BlockChain blockChain = BlockChain.createInstanceBlockChain();
-        blockChain.generateBlock();
-        blockChain.generateBlock();
-        blockChain.generateBlock();
-        blockChain.generateBlock();
-        blockChain.generateBlock();
+        blockChain.generateBlock(countZero);
+        blockChain.generateBlock(countZero);
+        blockChain.generateBlock(countZero);
+        blockChain.generateBlock(countZero);
+        blockChain.generateBlock(countZero);
 
         for (Block block: blockChain.getBlockChain()) {
             System.out.println(block);
